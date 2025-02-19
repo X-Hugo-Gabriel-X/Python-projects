25591bea0dd86f46e0c5b4d8ab9409ae307467f2
Snake-C4se <hugogabriegamr@gmail.com>
2025-02-19 20:10:39 -0300

    jogos em Python

---
+++ jogos.py
@@ -0,0 +1,22 @@
+import forca
+import advinhacao
+
+print("*********************************")
+print("********Escolha seu jogo!*********")
+print("*********************************")
+
+print("(1)forca (2)advinhação")
+
+jogo = int(input('Qual o jogo?'))
+
+if (jogo == 1):
+    print('jogando forca')
+    forca.jogar()
+elif(jogo == 2):
+    print('jogando dvinhação')
+    advinhacao.jogar()
+
+    if (__name__ == "__main__"):
+        escolhe_jogo()
+
+
